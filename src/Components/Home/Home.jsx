c69b4eb194650464d69f9eb1c8b10df21574ff92
Sowmitra Guha <sowmmochy@gmail.com>
2025-04-20 02:20:20 +0600

    core values card created and stled with tailwind css

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
         <>
             <div className='banner-container'>
                 <div className="heading">
-                    <h2 className='text-uppercase'>Discover Culinary Excellence</h2>
+                    <h2 className='text-uppercase text-5xl'>Discover Culinary Excellence</h2>
                     <p className='text-italic font-52'>with khadok</p>
                 </div>
                 <div className="badge-container">
--- src/Components/OurCoreValues/OurCoreValues.jsx
+++ src/Components/OurCoreValues/OurCoreValues.jsx
@@ -4,9 +4,9 @@ import ValueCard from '../ValueCard/ValueCard';
 
 const OurCoreValues = ({core_values}) => {
     return (
-        <div className='p-16'>
-            <h2 className='uppercase'>Our Core Values</h2>
-            <div className="core-values">
+        <div className='our-core-value bg-[#50591A] text-center'>
+            <h2 className='core-value-title uppercase text-[#FCC14F] font-bold text-3xl'>Our Core Values</h2>
+            <div className="core-values grid grid-cols-4 gap-4 ">
                 {
                     core_values.map(value => <ValueCard value={value}/>)
                 }
